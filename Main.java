089d68e9a763a28d2d0cc8b7fc05baeb721ed207
rickisurco <Maik.30@gmail.com>
2020-09-21 08:14:38 -0500

    semana 3

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import creación.Producto;
 import entities.Person;
 
@@ -7,23 +9,9 @@ import entities.Person;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO JAVA");
-        Person p = new Person();
-        p.setName("David "+p);
-        p.setSex("M");
-        System.out.println("sexo= "+ p.getSex() );
-        p.laugh();
-
-        Person p2 = new Person("Gloria","F");
-        System.out.println("sexo= "+ p2.getSex() );
-        p2.laugh();
-
-         System.out.println("---------------nuevo agregado----------------");
-        Producto proc=new Producto();
-        proc.setProducto("LAVADORA");
-        proc.setCosto("119.90 SOLES");
-        System.out.println("COSTO = "+ proc.getCosto() );
-        System.out.println("PRODUCTO = "+ proc.getProducto());
+        Scanner input=new Scanner(System.in);
+        String name=input.nextLine();
+        System.out.println("escribiste: "+name);
        
     }
 }
